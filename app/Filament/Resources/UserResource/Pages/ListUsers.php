6a5c7392ff4d2150c9e80f1f5bbd2c74f2bf513a
rizkifebriansyach-ibs <rizki.febriansyach@ib-synergy.co.id>
2025-05-09 16:18:58 +0700

    SCRUM-20 Modify table user, add navbar for choose role

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +17,24 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'admin' => Tab::make('Admins')
+                ->icon('heroicon-o-shield-check')
+                ->badge(fn() => User::where('role', 'admin')->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('role', 'admin')),
+            'customer' => Tab::make('Customers')
+                ->icon('heroicon-o-user-group')
+                ->badge(fn() => User::where('role', 'customer')->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('role', 'customer')),
+            'driver' => Tab::make('Drivers')
+                ->icon('heroicon-o-truck')
+                ->badge(fn() => User::where('role', 'driver')->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('role', 'driver')),
         ];
     }
 }
